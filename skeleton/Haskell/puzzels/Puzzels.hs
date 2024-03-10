f65317d0dbe8b8e7df754e2964cb83aa83dc7169
anh <anhle261137@gmail.com>
2024-03-10 11:45:36 +0100

    gevorderd

--- skeleton/Haskell/puzzels/Puzzels.hs
+++ skeleton/Haskell/puzzels/Puzzels.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use or" #-}
+{-# HLINT ignore "Use map" #-}
 module Puzzels where
 
 -- 1. length'
--- skeleton/Haskell/sudoku/SudokuSolver.hs
+++ skeleton/Haskell/sudoku/SudokuSolver.hs
@@ -5,7 +5,7 @@ type Row = Int
 type Column = Int
 type Value = Int
 type Grid = [[Value]] -- Only used to read/write from/to a file.
-type Sudoku = (Row,Column) -> Value
+type Sudoku = (Row, Column) -> Value
 type Constraint = (Row, Column, [Value])
 type Node = (Sudoku, [Constraint])
 
@@ -25,36 +25,104 @@ sud2grid :: Sudoku -> Grid
 sud2grid s = [[s (r, c) | c <- positions] | r <- positions]
 
 grid2sud :: Grid -> Sudoku
-grid2sud gr = \(r, c) -> pos gr (r, c)
-  where pos :: [[a]] -> (Row,Column) -> a
-        pos gr (r, c) = (gr !! (r - 1)) !! (c - 1)
+grid2sud gr (r, c) = gr !! (r - 1) !! (c - 1)
 
--- Extends a sudoku with a value at (row, column).
 extend :: Sudoku -> (Row, Column, Value) -> Sudoku
-extend sud (r, c, v) (i, j) = if r == i && c == j then v else sud (i, j)
+extend sud (r, c, v) (i, j) = if (r, c) == (i, j) then v else sud (i, j)
 
--- Read a file-sudoku with a Grid like format into a Sudoku.
 readSudoku :: String -> IO Sudoku
-readSudoku filename =
-    do stringGrid <- readFile filename
-       return $ (grid2sud . splitStringIntoGrid) stringGrid
-       where splitStringIntoGrid = map (map readint . words) . lines
-             readint x = read x :: Int
+readSudoku filename = grid2sud . splitStringIntoGrid <$> readFile filename
+    where splitStringIntoGrid = map (map readint . words) . lines
+          readint x = read x :: Int
 
-{- Prints a Sudoku to the terminal by transforming it to a grid first.
-   Do not modify this, or your tests will fail.
--}
 printSudoku :: Sudoku -> IO ()
 printSudoku = putStr . unlines . map (unwords . map show) . sud2grid
 
--- Helper to parse command-line arguments.
 getSudokuName :: [String] -> String
-getSudokuName [] = error "Filename of sudoku as first argument."
+getSudokuName [] = error "Filename of sudoku as the first argument."
 getSudokuName (x:_) = x
 
+freeInRow :: Sudoku -> Row -> [Value]
+freeInRow sud row = values \\ [sud (row, col) | col <- positions]
+
+freeInColumn :: Sudoku -> Column -> [Value]
+freeInColumn sud col = values \\ [sud (row, col) | row <- positions]
+
+freeInSubgrid :: Sudoku -> (Row, Column) -> [Value]
+freeInSubgrid sud (row, col) = values \\ [sud (r, c) | r <- blockRows, c <- blockCols]
+    where blockRows = getBlockIndices row
+          blockCols = getBlockIndices col
+          getBlockIndices x = let start = 3 * ((x - 1) `div` 3) + 1 in [start..start+2]
+
+freeAtPos :: Sudoku -> (Row, Column) -> [Value]
+freeAtPos sud (row, col) = intersect (freeInRow sud row) $ intersect (freeInColumn sud col) (freeInSubgrid sud (row, col))
+
+openPositions :: Sudoku -> [(Row, Column)]
+openPositions sud = [(r, c) | r <- positions, c <- positions, sud (r, c) == 0]
+
+rowValid :: Sudoku -> Row -> Bool
+rowValid sud row = let rowValues = [sud (row, col) | col <- positions] in nub rowValues == rowValues
+
+colValid :: Sudoku -> Column -> Bool
+colValid sud col = let colValues = [sud (row, col) | row <- positions] in nub colValues == colValues
+
+subgridValid :: Sudoku -> (Row, Column) -> Bool
+subgridValid sud (row, col) =
+  let subgridValues = [sud (r, c) | r <- blockRows, c <- blockCols]
+  in nub subgridValues == subgridValues
+  where
+    blockRows = getBlockIndices row
+    blockCols = getBlockIndices col
+    getBlockIndices x = let start = 3 * (x - 1) `div` 3 + 1 in [start..start+2]
+
+consistent :: Sudoku -> Bool
+consistent sud =
+  all (\(r, c) -> let blockRows' = blockRows r
+                      blockCols' = blockCols c
+                  in nub [sud (r', c') | r' <- blockRows', c' <- blockCols', sud (r', c') /= 0] ==
+                     [sud (r', c') | r' <- blockRows', c' <- blockCols', sud (r', c') /= 0])
+      [(r, c) | r <- centerOfBlocks, c <- centerOfBlocks]
+  where
+    blockRows x = let start = 3 * ((x - 1) `div` 3) + 1 in [start..start+2]
+    blockCols = blockRows
+
+constraints :: Sudoku -> [Constraint]
+constraints sud = [(r, c, freeAtPos sud (r, c)) | (r, c) <- openPositions sud]
+
+solveSudoku :: Sudoku -> IO Sudoku
+solveSudoku sud = do
+    let (result, steps) = solve (sud, constraints sud)
+    mapM_ (\(step, s) -> putStrLn $ "Step " ++ show step ++ ": " ++ s) (zip [1..] steps)
+    return result
+  where
+    solve :: Node -> (Sudoku, [String])
+    solve (s, []) = (s, [])
+    solve (s, cs) = tryConstraints (sortBy (\(_, _, vs) (_, _, vs') -> compare (length vs) (length vs')) cs)
+      where
+        tryConstraints [] = (s, [])
+        tryConstraints ((r, c, vs):cs') =
+            case vs of
+                [] -> (s, [])
+                _  -> tryValues vs
+          where
+            tryValues [] = (s, [])
+            tryValues (v:vs') =
+                case filter (consistent . fst) [(extend s (r, c, v), constraints (extend s (r, c, v))) | v <- vs] of
+                    []             -> tryValues vs'
+                    ((s', cs''):_) -> let (result, steps) = solve (s', cs'' ++ filter (\(r', c', _) -> (r', c') /= (r, c)) cs')
+                                    in (result, ("Trying value " ++ show v ++ " at position " ++ show (r, c)) : steps)
+
 main :: IO ()
-main =
-    do args <- getArgs
-       sud <- (readSudoku . getSudokuName) args
-       -- TODO: Call your solver.
-       printSudoku sud
+main = do
+    args <- getArgs
+    sud <- readSudoku (getSudokuName args)
+    
+    if consistent sud
+        then do
+            finalSudoku <- solveSudoku sud
+            printSudoku finalSudoku
+        else do
+            error "Unsolvable sudoku"
+
+    -- If the Sudoku is consistent and solvable, the program will continue.
+    -- If the Sudoku is inconsistent, the program will terminate with an error message.
